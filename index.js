6ee54f7f9ed128e2f03b6d3d8d185fc7d5301371
xandisan <xandisan1@hotmail.com>
2022-01-13 14:30:16 -0300

    Rota da raiz da index

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const res = require('express/lib/response')
 
 const app = express()
 
 app.listen(3000, () => console.log('servidor rodando na porta 3000'))
 
+app.get ('/', (req, res) => res.send('Rota raiz'))
 app.get ('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos'))
